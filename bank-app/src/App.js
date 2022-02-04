df3f38992ee03e58788cb85a80fd1f5d08504d0e
Graham Sabin <grahamcsabin@outlook.com>
2022-02-03 19:53:06 -0800

    initialized the node backend, currently setting up

--- bank-app/src/App.js
+++ bank-app/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar";
-//import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+    </Router>
   );
 }
 
